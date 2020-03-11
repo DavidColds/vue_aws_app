a9a191c963b607996e0800158117052b0e6eaae1
David Drughi <drughidavid6@gmail.com>
2020-03-11 12:31:12 +0100

    last commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,5 +2,7 @@
   <div class="about">
     <h1>Wellcome,</h1>
     <h2>You have successfully signed in</h2>
+        <amplify-sign-out></amplify-sign-out>
+
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
     <amplify-authenticator></amplify-authenticator>
-    <amplify-sign-out></amplify-sign-out>
   </div>
 </template>
 
