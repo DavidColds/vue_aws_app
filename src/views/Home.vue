1703f85a11196dfade0a555fba68eec377ae7354
David Drughi <drughidavid6@gmail.com>
2020-03-11 12:24:42 +0100

    added aws auth

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <amplify-authenticator></amplify-authenticator>
+    <amplify-sign-out></amplify-sign-out>
   </div>
 </template>
 
